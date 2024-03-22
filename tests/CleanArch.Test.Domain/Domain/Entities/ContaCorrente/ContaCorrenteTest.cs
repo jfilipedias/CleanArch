f88cf16b03ab66f23eeee33b76712181f6668764
Filipe Dias <filipediacsontato@gmail.com>
2024-03-22 13:44:03 -0300

    refactor: cria fixtures pros testes

---
+++ tests/CleanArch.Test.Domain/Domain/Entities/ContaCorrente/ContaCorrenteTest.cs
@@ -0,0 +1,54 @@
+﻿using CleanArch.Domain;
+
+namespace CleanArch.UnitTests;
+
+[Collection(nameof(ContaCorrenteTestFixtureCollection))]
+public class ContaCorrenteTest
+{
+    private readonly ContaCorrenteTestFixture _contaCorrenteTestFixture;
+
+    public ContaCorrenteTest(ContaCorrenteTestFixture contaCorrenteTestFixture)
+    {
+        _contaCorrenteTestFixture = contaCorrenteTestFixture;
+    }
+
+    [Fact]
+    [Trait("Domínio", "ContaCorrente")]
+    public void Debito_DeveAtualizarSaldo()
+    {
+        var contaCorrente = _contaCorrenteTestFixture.ObterContaCorrente(100_00, 0);
+        contaCorrente.Debitar(50_00);
+
+        Assert.Equal(50_00, contaCorrente.Saldo);
+    }
+
+    [Fact]
+    [Trait("Domínio", "ContaCorrente")]
+    public void Debito_PodeNegativarSaldoSeTiverLimite()
+    {
+        var contaCorrente = _contaCorrenteTestFixture.ObterContaCorrente(0, 100_00);
+        contaCorrente.Debitar(50_00);
+
+        Assert.Equal(-50_00, contaCorrente.Saldo);
+    }
+
+
+
+    [Fact]
+    [Trait("Domínio", "ContaCorrente")]
+    public void Debito_DeveLancarExcecaoCasoNaoTenhaLimiteDisponivel()
+    {
+        var contaCorrente = _contaCorrenteTestFixture.ObterContaCorrente(100_00, -99_00);
+        Assert.Throws<SaldoInsuficienteException>(() => contaCorrente.Debitar(2_00));
+    }
+
+    [Fact]
+    [Trait("Domínio", "ContaCorrente")]
+    public void Credito_DeveAdicionarSaldo()
+    {
+        var contaCorrente = _contaCorrenteTestFixture.ObterContaCorrente(0, 0);
+        contaCorrente.Creditar(50_00);
+
+        Assert.Equal(50_00, contaCorrente.Saldo);
+    }
+}
---
+++ tests/CleanArch.Test.Domain/Domain/Entities/ContaCorrente/ContaCorrenteTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using CleanArch.Domain;
+
+namespace CleanArch.UnitTests;
+
+[CollectionDefinition(nameof(ContaCorrenteTestFixtureCollection))]
+public class ContaCorrenteTestFixtureCollection : ICollectionFixture<ContaCorrenteTestFixture> { }
+
+public class ContaCorrenteTestFixture
+{
+    public ContaCorrente ObterContaCorrente(int saldo, int limite)
+    {
+        return new ContaCorrente()
+        {
+            Id = 1,
+            Saldo = saldo,
+            Limite = limite,
+        };
+    }
+}
