c80d58f11e45ef8498b9484043b609ed3196b71c
Filipe Dias <filipediacsontato@gmail.com>
2024-04-01 15:16:18 -0300

    feat: cria teste de validacao de etidade

---
+++ test/CleanArch.UnitTest/Domain/Entities/TransacaoTest.cs
@@ -0,0 +1,17 @@
+﻿using CleanArch.Domain;
+
+namespace CleanArch.UnitTest;
+
+public class TransacaoTest
+{
+    [Fact]
+    public void Validar_DeveNotificarTipoInvalido()
+    {
+        var action = () => new Transacao("pix", "z", 1_00);
+        var ex = Assert.Throws<NotificacaoException>(action);
+
+        Assert.Single(ex.Erros);
+        Assert.Equal("Tipo", ex.Erros.First().Propriedade);
+        Assert.Equal("O tipo da transação deve ser 'c' ou 'd'.", ex.Erros.First().Mensagem);
+    }
+}
